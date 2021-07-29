b3863ac4aa6cb65882cd764490a624cb0c06da20
Mohammed Ali Chherawalla <mohammed.ali.chherawalla@gmail.com>
2021-07-30 00:49:12 +0530

    Initial commit

---
+++ jest.setup.js
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/',
+    search: '',
+    hash: '',
+    state: null,
+    key: '5nvxpbdafa'
+  }),
+  useHistory: jest.fn().mockReturnValue({
+    length: 2,
+    action: 'POP',
+    location: {
+      pathname: '/',
+      search: '',
+      hash: ''
+    }
+  })
+}));
+
+Object.defineProperty(window, 'matchMedia', {
+  value: jest.fn(() => {
+    return {
+      matches: true,
+      addListener: jest.fn(),
+      removeListener: jest.fn()
+    };
+  })
+});
